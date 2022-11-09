eaf6abb80e5a9a7e7b74c65497b73f74aa2e8465
bchaindeveloper <bcdev3@protonmail.com>
2022-11-09 17:38:42 -0500

    Images

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,3 +1,8 @@
+import { HiMenuAlt4 } from 'react-icons/h1';
+import { AiOutlineClose } from 'react-icons/ai';
+
+import logo from '../images/logo.png';
+
 const Navbar = () => {
     return (
         <h1>Navbar</h1>
