def3932ddee67fab860e1488c3710f611efd73f5
bchaindeveloper <bcdev3@protonmail.com>
2022-11-14 17:11:14 -0500

    TransactionContext.jsx

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x56765189eF043b5c888065225cd13aE6E326635a';
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/91ZEAx5S_y4xj9Jz0YFGwgXGQ6vEsxl4
+
+require("@nomiclabs/hardhat-waffle");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/91ZEAx5S_y4xj9Jz0YFGwgXGQ6vEsxl4',
+      accounts: [ PRIVATE_KEY ]
+    }
+  }
 };
