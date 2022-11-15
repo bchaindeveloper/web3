9f9fc73899d2a27d194c816b644e1359e3efea8f
bchaindeveloper <bcdev3@protonmail.com>
2022-11-15 13:27:56 -0500

    Send Transaction

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
   networks: {
     goerli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/91ZEAx5S_y4xj9Jz0YFGwgXGQ6vEsxl4',
-      accounts: [ PRIVATE_KEY ]
+      accounts: [ 'ca918f52145735043b8a3dc17ed08d1ffe897b3466c6649051c32d9c353e6872' ]
     }
   }
 };
