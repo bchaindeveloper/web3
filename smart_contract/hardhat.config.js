3837fc700352dd6f1df39148aa8a1f7b0c6f982e
bchaindeveloper <bcdev3@protonmail.com>
2022-11-16 10:47:30 -0500

    PK

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,14 +1,19 @@
 // https://eth-goerli.g.alchemy.com/v2/91ZEAx5S_y4xj9Jz0YFGwgXGQ6vEsxl4
 
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+
+
 module.exports = {
   solidity: "0.8.0",
   networks: {
     goerli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/91ZEAx5S_y4xj9Jz0YFGwgXGQ6vEsxl4',
-      accounts: [ 'ca918f52145735043b8a3dc17ed08d1ffe897b3466c6649051c32d9c353e6872' ]
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
     }
   }
 };
